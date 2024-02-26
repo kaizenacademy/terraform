1be3deba73a36676ddf9f948c20cc589ee72ec27
kaizenacademy <kaizenacademy@gmail.com>
2024-02-26 21:45:24 +0000

    added additonal vm

--- DO-vm-creation/main.tf
+++ DO-vm-creation/main.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_droplet" "master" {
   name   = "ubuntu-master"
-  size   = "s-1vcpu-1gb"
-  image  = "ubuntu-20-04-x64"
+  size   = var.master_size
+  image  = "ubuntu-22-04-x64"
   region = "nyc3"
   ssh_keys = [
     var.ssh_fingerprint_1,
@@ -10,9 +10,20 @@ resource "digitalocean_droplet" "master" {
 }
 
 resource "digitalocean_droplet" "worker1" {
-  name   = "ubuntu-worker"
-  size   = "s-1vcpu-1gb"
-  image  = "ubuntu-20-04-x64"
+  name   = "ubuntu-worker1"
+  size   = var.worker1_size
+  image  = "ubuntu-22-04-x64"
+  region = "nyc3"
+  ssh_keys = [
+    var.ssh_fingerprint_1,
+    var.ssh_fingerprint_2,
+  ]
+}
+
+resource "digitalocean_droplet" "worker2" {
+  name   = "ubuntu-worker2"
+  size   = var.worker2_size
+  image  = "ubuntu-22-04-x64"
   region = "nyc3"
   ssh_keys = [
     var.ssh_fingerprint_1,
--- DO-vm-creation/variable.tf
+++ DO-vm-creation/variable.tf
@@ -12,3 +12,19 @@ variable "ssh_fingerprint_2" {
   description = "SSH Key Fingerprint 2"
   type        = string
 }
+
+variable master_size {
+  description = "size of master node"
+  type        = string
+}
+
+variable worker1_size {
+  description = "size of worker1 node"
+  type        = string
+}
+
+variable worker2_size {
+  description = "size of worker2 node"
+  type        = string
+}
+
